2eccbd6110502303c35ad2c418f82ac4f3d92565
Guillaume Huertas <guillaumehuertas@free.fr>
2019-07-03 14:51:02 +0200

    Ajout de commentaire à Installeur.sh

--- Guillaume/Installeur.sh
+++ Guillaume/Installeur.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Script permetant d'installer tous les programme nécesssaire pour le générateur de code
 read -p "Avez-vous Node.js, java jdk8, Curl et git installé ? (Y/n) " response
 
 if [ -z $response ] ; then
@@ -13,19 +14,15 @@ read -p "Avez-vous Node.js, java jdk8, Curl et git installé ? (Y/n) " response
 done
 
 if [ $response == "n" ] ; then
-
-	 ./installations/installationAll.sh
-
+	# Lance le script installationAll. sh qui installe tout les programme nécessaires
+	./installations/installationAll.sh
 else 
-
 	echo "J'espère que tout est bien installé !"
-
 fi
 
+# Préviens l'utilisateur que le programme va lancer un script de contrôle
 echo "Nous allons vérifier que vous dites la vérité !"
-
 ./controleur.sh
-
 echo "fini"
 
 
