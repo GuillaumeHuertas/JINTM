90ec0980a3525f1a9bff542bb18c54a9a39c1d98
Guillaume Huertas <guillaumehuertas@free.fr>
2019-07-03 15:43:27 +0200

    Message de couleur pour les installations

--- Guillaume/controleur.sh
+++ Guillaume/controleur.sh
@@ -10,66 +10,78 @@ java -version 2>/dev/null 1>/dev/null
 # Si le code retours est différents de 0 un message nous averti
 # que Java n'est pas installlé
 if [ $? != 0 ] ; then
-	 echo "Java n'est pas installé !"
+     # Affiche le message en Jaune
+     echo -e "\e[93mJava n'est pas installé\e[0m"
      # Appel le script d'installation de OpenJDK8
      ./installations/installationOpenJDK8.sh
 else 
-	echo "Java est bien installé ! "
+     # Affiche le message en Vert
+     echo -e "\e[92mJava est bien installé !\e[0m"
 fi
 
 # Demande la version de Javac
 javac -version 2>/dev/null >/dev/null
 # Idem 
 if [ $? != 0 ] ; then
-	 echo "Javac n'est pas installé !"
+     # Affiche le message en Jaune
+     echo -e "\e[93mJavac n'est pas installé !\e[0m"
      # Appel le script d'installation de OpenJDK8
      ./installations/installationOpenJDK8.sh
 else 
-	echo "Javac est bien installé ! "
+     # Affiche le message en Vert
+     echo -e "\e[92mJavac est bien installé !\e[0m"
 fi
 
 # Demande la version de Git
 git version >/dev/null 2>/dev/null
 # Idem 
 if [ $? != 0 ] ; then
-	 echo "Git n'est pas installé !"
+     # Affiche le message en Jaune
+     echo -e "\e[93mGit n'est pas installé !\e[0m"
      # Appel le script d'installation de Git
      ./installations/installationGit.sh
 else 
-	echo "Git est bien installé ! "
+     # Affiche le message en Vert
+     echo -e "\e[92mGit est bien installé !\e[0m"
 fi
 
 # Demande la version de Curl
 curl --version >/dev/null 2>/dev/null
 # Idem 
 if [ $? != 0 ] ; then
-	 echo "Curl n'est pas installé !"
+     # Affiche le message en Jaune
+     echo -e "\e[93mCurl n'est pas installé !\e[0m"
      # Appel le script d'installation de Curl
      ./installations/installationCurl.sh
 else 
-	echo "Curl est bien installé ! "
+     # Affiche le message en Vert
+     echo -e "\e[92mCurl est bien installé !\e[0m"
 fi
 
 # Demande la version de node.js
 node -v >/dev/null 2>/dev/null
 # Idem 
 if [ $? != 0 ] ; then
-	 echo "node.js n'est pas installé !"
+     # Affiche le message en Jaune
+     echo -e "\e[93mnode.js n'est pas installé !\e[0m"
      # Appel le script d'installation de Node.js
      ./installations/installationNode.sh
 else 
-	echo "node.js est bien installé ! "
+     # Affiche le message en Vert
+     echo -e "\e[92mnode.js est bien installé !\e[0m"
 fi
 
 # Demande la version de npm
 npm -v >/dev/null 2>/dev/null
 # Idem 
 if [ $? != 0 ] ; then
-	 echo "npm n'est pas installé !"
+     # Affiche le message en Jaune
+     echo -e "\e[93mnpm n'est pas installé !\e[0m"
      # Appel le script d'installation de Node.js
      ./installations/installationNode.sh
 else 
-	echo "npm est bien installé ! "
+     # Affiche le message en Vert
+     echo -e "\e[92mnpm est bien installé !\e[0m"
 fi
 
 
--- Guillaume/installations/installationNode.sh
+++ Guillaume/installations/installationNode.sh
@@ -2,7 +2,7 @@
 
 # Ce script permet d'installer openJDK8
 
-read -p "Voulez-vous installer open JDK8  (y/N) " response1
+read -p "Voulez-vous installer Node.js (comprend npm) (y/N) " response1
 
 # Si la réponse est vide donne la valeur N à la variable N (réponse par défaut)
 if [ -z $response1 ] ; then
@@ -13,7 +13,7 @@ fi
 while [[ "$response1" != "y" && "$response1" != "N" ]]
 do
 echo "Choisissez y ou N"
-read -p "Voulez-vous installer open JDK8  (y/N) " response1
+read -p "Voulez-vous installer Node.js (comprend npm) (y/N) " response1
 done
 
 # Si la réponse est y installe et met-à-jour les paquets
